835f7eb7bfaffafe804abfaa2b2e5b4cf5181fc0
Augusto Barros <126268126+augusto-barros@users.noreply.github.com>
2024-09-20 09:24:44 -0600

    Week 2 Assignment
    Week 2 Assignment and update to the main page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>Augusto Antunes Barros</h1>
+            <p><Link href="https://github.com/augusto-barros/cprg306-assignments">GitHub Repository</Link></p>
+        </main>
+    );
+}
