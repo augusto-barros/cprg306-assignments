f686a69a5a7e87784e392afde7ebf924e73e3aa9
Augusto Barros <126268126+augusto-barros@users.noreply.github.com>
2024-11-07 13:36:09 -0700

    Update item.js

--- app/week-8/item.js
+++ app/week-8/item.js
@@ -3,8 +3,7 @@ const Item = ({ name, quantity, category, onSelect }) => {
     return (
         <li 
             onClick={onSelect}
-            className="cursor pointer bg-indigo-700 p-4 w-64 mx-auto rounded-lg flex flex-col items-center hover:bg-indigo-800"
-            >
+            className="cursor pointer bg-indigo-700 p-4 w-64 mx-auto rounded-lg flex flex-col items-center hover:bg-indigo-800">
             <p className="font-bold">{name}</p>
             <p>Buy {quantity} in {category}</p>
         </li>
